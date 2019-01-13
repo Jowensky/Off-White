7d9ab85f19f110edd9c2a72975c75844a749de8e
Jowensky Neard <jp.12@live.com>
2019-01-13 17:36:25 -0500

    model and scraper set

---
+++ controllers/scrape.js
@@ -0,0 +1,48 @@
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+axios
+  .get("https://www.off---white.com/en/US/men/t/seasons/ss2019")
+  .then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+      result.image = $(this)
+        .children("a")
+        .children("figure")
+        .children("img")
+        .attr("src");
+      result.name = $(this)
+        .children("a")
+        .children("figure")
+        .children("figcaption")
+        .children(".brand-name")
+        .text();
+      result.category = $(this)
+        .children("a")
+        .children("figure")
+        .children("figcaption")
+        .children(".category-and-season")
+        .children(".category")
+        .text();
+      result.price = $(this)
+        .children("a")
+        .children("figure")
+        .children("figcaption")
+        .children(".price")
+        .children("span")
+        .children("strong")
+        .text();
+
+      console.log(result);
+    });
+  });
---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect(
+  "mongodb://localhost/OffWhite",
+  { useNewUrlParser: true }
+);
+
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
+  
